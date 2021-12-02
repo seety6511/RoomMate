29ef64fd9b5a968715239df8ed1ded3a3736d54d
김희진 <67904074+Rrrriaa@users.noreply.github.com>
2021-12-02 17:19:31 +0900

    1202

--- Assets/KHJ/02.Scripts/KHJ_Diary.cs
+++ Assets/KHJ/02.Scripts/KHJ_Diary.cs
@@ -1,19 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
+[RequireComponent(typeof(AudioSource))]
 public class KHJ_Diary : MonoBehaviour
 {
+	public static KHJ_Diary instance;
 	public bool isOpened;
 	public GameObject Lock;
+	public KHJ_DiaryButton[] buttons;
 	public AnimatedBookController bookController;
-	void Start()
+	public GameObject PasswordHint;
+	public bool able;
+
+	public AudioClip clickSound;
+	public AudioClip correctSound;
+	public AudioClip incorrectSound;
+	AudioSource source; 
+
+	string Answer;
+	public List<int> AnswerList;
+	public List<int> ButtonInputList;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+			instance = this;
+        }
+        else
+        {
+			Destroy(gameObject);
+        }
+    }
+    void Start()
 	{
 		bookController = GetComponent<AnimatedBookController>();
-	}
+		Answer = PasswordHint.GetComponent<MText.Modular3DText>().Text;
+		source = GetComponent<AudioSource>();
 
+		for (int i =0; i< Answer.ToString().Length; i++)
+        {
+			AnswerList.Add(int.Parse(Answer[i].ToString()));
+        }
+		able = true;
+	}
 	void Update()
 	{
+		//if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+		if (Input.GetButtonDown("Fire1") && able)
+		{
+			//Ray ray = new Ray(trRight.position, trRight.forward);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+			if (Physics.Raycast(ray, out hitInfo, float.MaxValue))
+			{			
+                if (hitInfo.collider.name.Contains("KeyPad"))
+                {
+					hitInfo.collider.gameObject.GetComponent<KHJ_DiaryButton>().ClickButton();
+                }
+			}
+		}
+
+		//셋팅
 		if (!isOpened)
         {
 			Lock.SetActive(true);
@@ -23,19 +72,54 @@ public class KHJ_Diary : MonoBehaviour
         {
 			Lock.SetActive(false);
         }
-
+		//조작
 		if (Input.GetKeyDown(KeyCode.RightArrow))
 		{
-			bookController.TurnToPreviousPage();
+			TurnPrePage();
 		}
 		else if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
-			bookController.TurnToNextPage();
+			TurnNextPage();
 		}
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-			bookController.CloseBook();
+			CloseBook();
         }
+	}
+	public void TurnPrePage()
+    {
+		bookController.TurnToPreviousPage();
+	}
+	public void TurnNextPage()
+    {
+		bookController.TurnToNextPage();
+	}
+	public void CloseBook()
+    {
+		bookController.CloseBook();
+	}
+
+	public bool CheckAnswer()
+    {
+		bool check = false;
+		List<int> duplicates = ButtonInputList.Intersect(AnswerList).ToList();
+		if (duplicates.Count == AnswerList.Count)
+			check = true;
+
+		return check;
+    }
+
+	public void ClearBtn()
+    {
+		ButtonInputList.Clear();
+		foreach(KHJ_DiaryButton btn in buttons)
+        {
+			btn.ClearButton();
+        }
+    }
+	public void PlaySound(AudioClip clip)
+    {
+		source.PlayOneShot(clip);
 	}
 }
---
+++ Assets/KHJ/02.Scripts/KHJ_DiaryButton.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class KHJ_DiaryButton : MonoBehaviour
+{
+    public Image bgImage;
+    KHJ_Diary diary;
+    public int num;
+    void Start()
+    {
+        diary = KHJ_Diary.instance;
+        bgImage = GetComponentInChildren<Image>();
+    }
+    public void ClickButton()
+    {
+        if(num == 0)
+        {
+            //초기화 버튼이면 초기화
+            diary.ClearBtn();
+            return;
+        }
+        //아니면 입력값 넣기
+        if (!diary.ButtonInputList.Contains(num))
+        {
+            diary.ButtonInputList.Add(num);
+            BtnInputEft();
+            //비밀번호 길이만큼 입력이 끝났다면 확인
+            if (diary.ButtonInputList.Count == diary.AnswerList.Count)
+            {
+                diary.able = false;
+                StartCoroutine(waitforsecond());
+            }
+        }
+    }
+    IEnumerator waitforsecond()
+    {
+        yield return new WaitForSeconds(1);
+        if (diary.CheckAnswer())
+        {
+            //맞으면 열림
+            diary.PlaySound(diary.correctSound);
+            diary.isOpened = true;
+            diary.TurnPrePage();
+        }
+        else
+        {
+            //틀리면 초기화
+            diary.PlaySound(diary.incorrectSound);
+            diary.ClearBtn();
+            diary.able = true;
+        }
+    }
+    public void BtnInputEft()
+    {
+        //색 바뀌기
+        bgImage.color = Color.gray;
+        diary.PlaySound(diary.clickSound);
+    }
+    public void ClearButton()
+    {
+        bgImage.color = Color.white;
+    }
+}
