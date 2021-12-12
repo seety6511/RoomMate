4d1e1a64201b62afe00d3f9b50ab837613f9837c
seety105 <seety6511@gmail.com>
2021-12-12 23:56:56 +0900

    1212_02

---
+++ Assets/SH_PageControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SH_PageControl : MonoBehaviour
+{
+    AnimatedBookController abc;
+
+    private void Awake()
+    {
+        abc = FindObjectOfType<AnimatedBookController>();
+    }
+
+    public enum PageDir
+    {
+        Next,
+        Prev,
+    }
+    public PageDir dir;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Hand") || other.gameObject.name != "Tip")
+            return;
+
+        switch (dir)
+        {
+            case PageDir.Next:
+                abc.TurnToNextPage();
+                break;
+
+            case PageDir.Prev:
+                abc.TurnToPreviousPage();
+                break;
+        }
+    }
+}
