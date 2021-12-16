3d8200d7337386ad23bf98a27e66e81ec4d0f23b
allelaok <88119690+allelaok@users.noreply.github.com>
2021-12-16 17:21:04 +0900

    Revert "Merge branch 'NSR'"
    This reverts commit f6f9aa86ff4b6e1946a764114e4f06f946735107, reversing
    changes made to 2ae0112e1d2cb606d522f3382cd559bb5c4259be.

--- Assets/AutoHand/Examples/Scenes/Oculus Integration/Scripts/OVRHandPlayerControllerLink.cs
+++ Assets/AutoHand/Examples/Scenes/Oculus Integration/Scripts/OVRHandPlayerControllerLink.cs
@@ -17,23 +17,29 @@ namespace Autohand.Demo{
 
         Vector2 moveInput;
         float turnInput;
-        public void Update()
+        public void Update() 
         {
             if (PhotonNetwork.IsConnected)
             {
+
                 if (NSR_AutoHandManager.instance.bodyplayer)
                 {
                     moveInput = OVRInput.Get(moveAxis, moveController);
                     turnInput = OVRInput.Get(turnAxis, turnController).x;
                 }
-                else
-                {
-                    if (NSR_AutoBodyPlayer.instance != null)
-                    {
-                        moveInput = NSR_AutoBodyPlayer.instance.recieve_moveInput;
-                        turnInput = NSR_AutoBodyPlayer.instance.recieve_turnInput;
-                    }
-                }
+                //else
+                //{
+                //    if(NSR_AutoBodyPlayer.instance != null)
+                //    {
+                //        moveInput = NSR_AutoBodyPlayer.instance.recieve_moveInput;
+                //        turnInput = NSR_AutoBodyPlayer.instance.recieve_turnInput;
+                //    }
+                //}
+            }
+            else
+            {
+                moveInput = OVRInput.Get(moveAxis, moveController);
+                turnInput = OVRInput.Get(turnAxis, turnController).x;
             }
 
             player.Move(moveInput);
