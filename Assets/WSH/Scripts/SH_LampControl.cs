bf519ca10c021dbaf046ab70a9920e06a08879b3
seety105 <seety6511@gmail.com>
2021-12-19 23:00:39 +0900

    1219

--- Assets/WSH/Scripts/SH_LampControl.cs
+++ Assets/WSH/Scripts/SH_LampControl.cs
@@ -1,30 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SH_LampControl : MonoBehaviour
 {
     public GameObject lightObj;
-    public Material bulbMat;    
+    public MeshRenderer mesh;
+    string proName = "_EmissionColor";
 
     public void OnOff()
     {
         lightObj.SetActive(!lightObj.activeSelf);
-        var property = bulbMat.GetColor("Emission");
+        var meshMaterials = mesh.materials;
 
+        Material result = null;
+        for(int i =0;i< meshMaterials.Count();++i)
+        {
+            var s = meshMaterials[i].name.Split('_');
+            if (s[0] == "Lamp")
+            {
+                result = meshMaterials[i];
+                break;
+            }
+        }
+        var w = result.shader.FindPropertyIndex(proName);
         if (lightObj.activeSelf)
         {
-            if (property != null)
-                bulbMat.SetColor("Emission", Color.white);
+            if (w != -1)
+                result.SetColor(proName, Color.white);
             else
-                bulbMat.color = Color.white;
+                result.color = Color.white;
         }
         else
         {
-            if (property != null)
-                bulbMat.SetColor("Emission", Color.black);
+            if (w != -1)
+                result.SetColor(proName, Color.black);
             else
-                bulbMat.color = Color.black;
+                result.color = Color.black;
         }
     }
 }
