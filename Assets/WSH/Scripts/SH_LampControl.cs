0fd5f44249bd8b09a214bfcd4ebb349f1c2df37a
seety6511 <seety6511@gmail.com>
2021-12-21 17:11:37 +0900

    1221

--- Assets/WSH/Scripts/SH_LampControl.cs
+++ Assets/WSH/Scripts/SH_LampControl.cs
@@ -7,8 +7,7 @@ using Photon.Pun;
 
 public class SH_LampControl : MonoBehaviourPun
 {
-    public GameObject lightObj;
-    public MeshRenderer mesh;
+    public List<GameObject> lightObj;
     string proName = "_EmissionColor";
 
     public void Grab()
@@ -19,33 +18,37 @@ public class SH_LampControl : MonoBehaviourPun
     [PunRPC]
     void OnOff()
     {
-        lightObj.SetActive(!lightObj.activeSelf);
-        var meshMaterials = mesh.materials;
+        foreach (var light in lightObj)
+        {
+            light.SetActive(!light.activeSelf);
 
-        Material result = null;
-        for(int i =0;i< meshMaterials.Count();++i)
-        {
-            var s = meshMaterials[i].name.Split('_');
-            if (s[0] == "Lamp")
+            var meshMaterials = light.GetComponent<MeshRenderer>().materials;
+
+            Material result = null;
+            for (int i = 0; i < meshMaterials.Count(); ++i)
             {
-                result = meshMaterials[i];
-                break;
+                var s = meshMaterials[i].name.Split('_');
+                if (s[0] == "Lamp")
+                {
+                    result = meshMaterials[i];
+                    break;
+                }
             }
-        }
-        var w = result.shader.FindPropertyIndex(proName);
-        if (lightObj.activeSelf)
-        {
-            if (w != -1)
-                result.SetColor(proName, Color.white);
+            var w = result.shader.FindPropertyIndex(proName);
+            if (light.activeSelf)
+            {
+                if (w != -1)
+                    result.SetColor(proName, Color.white);
+                else
+                    result.color = Color.white;
+            }
             else
-                result.color = Color.white;
-        }
-        else
-        {
-            if (w != -1)
-                result.SetColor(proName, Color.black);
-            else
-                result.color = Color.black;
+            {
+                if (w != -1)
+                    result.SetColor(proName, Color.black);
+                else
+                    result.color = Color.black;
+            }
         }
     }
 }
