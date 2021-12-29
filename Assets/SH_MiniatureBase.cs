bdc49244032b5fdb9bca3326e096c6e93e97e212
seety105 <seety6511@gmail.com>
2021-12-29 23:41:35 +0900

    1229

---
+++ Assets/SH_MiniatureBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SH_MiniatureBase : MonoBehaviour
+{
+    public GameObject leftTopAnchor;
+    public GameObject rightTopAnchor;
+    public GameObject rightBottomAnchor;
+
+    public GameObject gridTilePrefab;
+
+
+}
