7d447e42894cfad93a0dce4e7c1537df4dccda28
seety6511 <seety6511@gmail.com>
2022-01-02 23:21:16 +0900

    0102

---
+++ Assets/WSH/Scripts/SH_MiniatureBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SH_MiniatureBase : MonoBehaviour
+{
+    public GameObject miniatureGridTilePrefab;
+
+    public int width;
+    public int height;
+}
