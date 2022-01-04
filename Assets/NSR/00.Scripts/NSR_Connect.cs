a26aeb3a1689bcc32a5e5ddf188bd89f1da79990
allelaok <88119690+allelaok@users.noreply.github.com>
2022-01-04 15:38:41 +0900

    .

--- Assets/NSR/00.Scripts/NSR_Connect.cs
+++ Assets/NSR/00.Scripts/NSR_Connect.cs
@@ -49,65 +49,66 @@ public class NSR_Connect : MonoBehaviourPunCallbacks
         base.OnConnectedToMaster();
         PhotonNetwork.JoinLobby();
     }
-
-    //로비진입 성공 시 방 입장 시도
-    RoomOptions roomOptions;
+    
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
         print("로비 진입 성공");
-
-
-        roomOptions = new RoomOptions();
-        //roomOptions.PublishUserId = true;
-        //인원수 제한
-        roomOptions.MaxPlayers = 2;
     }
 
     bool isMaster;
     private void Update()
     {
-        if (PhotonNetwork.InLobby)
+
+        if (openCreateDoor)
         {
-            if (openCreateDoor)
+            isMaster = true;
+
+            RoomOptions roomOptions = new RoomOptions();
+            //roomOptions.PublishUserId = true;
+            //인원수 제한
+            roomOptions.MaxPlayers = 2;
+            PhotonNetwork.CreateRoom("1234", roomOptions, TypedLobby.Default);
+            openCreateDoor = false;
+        }
+        else if (enterBtn)
+        {
+            print(answer);            
+            enterBtn = false;
+
+            for(int i = 0; i < rooms.Count; i++)
             {
-                isMaster = true;
-                PhotonNetwork.CreateRoom("1234", roomOptions, TypedLobby.Default);
-                openCreateDoor = false;
-            }
-            else if (enterBtn)
-            {
-                print(answer);
-                PhotonNetwork.JoinRoom(answer);
-                enterBtn = false;
+                if(rooms[i].Name==answer)
+                {
+                    threshold.SetActive(false);
+                    joinSuccessText.SetActive(true);
+                    break;
+                }
             }
         }
 
-        if (PhotonNetwork.InRoom)
+        if (openJoinDoor)
         {
-            if (openJoinDoor)
-            {
-                PhotonNetwork.LoadLevel("KHJ_Test");
-                openJoinDoor = false;
-            }
+            PhotonNetwork.JoinRoom(answer);
+            openJoinDoor = false;
         }
+
+        //if (PhotonNetwork.InRoom)
+        //{
+        //    if (openJoinDoor)
+        //    {
+        //        PhotonNetwork.LoadLevel("KHJ_Test");
+        //        openJoinDoor = false;
+        //    }
+        //}
     }
     //방 입장 성공시
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
         print("방입장 완료");
-
-        if (isMaster)
-        {
-            PhotonNetwork.LoadLevel("KHJ_Test");
-            isMaster = false;
-        }
-        else
-        {
-            threshold.SetActive(false);
-            joinSuccessText.SetActive(true);
-        }
+        PhotonNetwork.LoadLevel("KHJ_Test");
+        
     }
 
     public override void OnJoinRoomFailed(short returnCode, string message)
@@ -116,25 +117,6 @@ public class NSR_Connect : MonoBehaviourPunCallbacks
 
         joinFailText.SetActive(true);
         threshold.SetActive(true);
-
-        if (PhotonNetwork.IsConnected == false)
-        {
-            //게임 버전 설정
-            PhotonNetwork.GameVersion = "1";
-            //접속 시도 (name서버 -> master서버)
-            PhotonNetwork.ConnectUsingSettings();
-        }
-        else if (PhotonNetwork.InLobby == false)
-        {
-            PhotonNetwork.JoinLobby();
-        }
-
-        if (enterBtn)
-        {
-            print(answer);
-            PhotonNetwork.JoinRoom(answer);
-            enterBtn = false;
-        }
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -143,6 +125,13 @@ public class NSR_Connect : MonoBehaviourPunCallbacks
 
         createFailText.SetActive(true);
         // 문 위치 원래대로
+    }
+
+    List<RoomInfo> rooms = new List<RoomInfo>();
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        rooms = roomList;
     }
 }
 
