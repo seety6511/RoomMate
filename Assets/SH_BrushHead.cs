5f847c27622b7296bfea5864872579b87b1b4944
seety105 <seety6511@gmail.com>
2022-01-16 03:05:14 +0900

    0116

--- Assets/SH_BrushHead.cs
+++ Assets/SH_BrushHead.cs
@@ -7,11 +7,14 @@ public class SH_BrushHead : MonoBehaviour
     [SerializeField]
     PaintColor buriedColor;
 
+    public PaintColor GetColor => buriedColor;
+
     Material fur;
 
     private void Awake()
     {
         fur = GetComponent<MeshRenderer>().material;
+        buriedColor = PaintColor.None;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,18 +27,23 @@ public class SH_BrushHead : MonoBehaviour
         {
             case PaintColor.Red:
                 fur.SetColor("_BaseColor",Color.red);
+                buriedColor = PaintColor.Red;
                 break;
             case PaintColor.Green:
                 fur.SetColor("_BaseColor", Color.green);
+                buriedColor = PaintColor.Green;
                 break;
             case PaintColor.Blue:
                 fur.SetColor("_BaseColor", Color.blue);
+                buriedColor = PaintColor.Blue;
                 break;
             case PaintColor.Black:
                 fur.SetColor("_BaseColor", Color.black);
+                buriedColor = PaintColor.Black;
                 break;
             case PaintColor.White:
                 fur.SetColor("_BaseColor", Color.white);
+                buriedColor = PaintColor.White;
                 break;
         }
     }
--- Assets/WSH/Scripts/SH_PaintTube.cs
+++ Assets/WSH/Scripts/SH_PaintTube.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum PaintColor
 {
+    None,
     Red,
     Green,
     Blue,
